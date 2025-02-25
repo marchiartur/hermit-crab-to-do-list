22b3911e4bcc4654b94e03d2a70a23dfd38af561
Artur <marchiartur@gmail.com>
2025-02-25 15:22:48 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'HermitCrab To-do list',
+  webDir: 'dist'
+};
+
+export default config;
